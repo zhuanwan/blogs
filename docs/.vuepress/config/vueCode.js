32df7d6bd176f427f9a515a3ae886ad8163ff604
wuhui <maniwu@aijiatui.com>
2020-12-09 16:51:59 +0800

    up

--- docs/.vuepress/config/vueCode.js
+++ docs/.vuepress/config/vueCode.js
@@ -1,11 +1,11 @@
 module.exports = [
-  "/vueCode/1-简单双向绑定.md",
-  "/vueCode/2-watcher&&dep.md",
-  "/vueCode/3-vue.use.md",
-  "/vueCode/4-vue.init.md",
   "/vueCode/React使用中遇到的一些问题.md",
   "/vueCode/Vue使用中遇到的一些问题.md",
+  "/vueCode/vue.init.md",
+  "/vueCode/vue.use.md",
   "/vueCode/vuex源码解析.md",
+  "/vueCode/watcher&&dep.md",
   "/vueCode/代码片段.md",
+  "/vueCode/简单双向绑定.md",
   "/vueCode/纯函数.md"
 ]
