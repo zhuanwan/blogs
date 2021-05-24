05ef001dc7ad47bda985b4d3eb82e4cbb2d67278
wuhui <maniwu@aijiatui.com>
2021-05-24 11:56:20 +0800

    up

--- docs/.vuepress/config/jsBlock.js
+++ docs/.vuepress/config/jsBlock.js
@@ -1,10 +1,14 @@
 module.exports = [
   "/jsBlock/parseInt.md",
   "/jsBlock/preventDefault报错.md",
+  "/jsBlock/promise.md",
   "/jsBlock/requestIdleCallback.md",
   "/jsBlock/undefined.md",
+  "/jsBlock/代码块.md",
   "/jsBlock/尾递归.md",
+  "/jsBlock/执行上下文.md",
   "/jsBlock/控制台.md",
   "/jsBlock/查找文件中的图片并下载.md",
-  "/jsBlock/树.md"
+  "/jsBlock/树.md",
+  "/jsBlock/深度广度优先.md"
 ]
