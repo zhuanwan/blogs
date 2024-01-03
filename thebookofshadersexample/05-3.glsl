342167902de20d004df6706d54fe6e8b5454ef5e
wu.hui <wu.hui@qkupower.com>
2024-01-03 14:20:39 +0800

    deploy

---
+++ thebookofshadersexample/05-3.glsl
@@ -0,0 +1,28 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float plot(vec2 st, float pct){
+  return  smoothstep( pct-0.02, pct, st.y) -
+          smoothstep( pct, pct+0.02, st.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+
+    // x轴左边小于0.5，返回0，x轴右边大于0.5，返回1
+    float y = step(0.5,st.x);
+
+    // x轴左边vec3(0,0,0)黑色 x轴右边vec3(255,255,255)白色
+    vec3 color = vec3(y);
+
+    float pct = plot(st,y);
+    color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+
+    gl_FragColor = vec4(color,1.0);
+}
